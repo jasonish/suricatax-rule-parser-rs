f5931e7448f8f5df8dd97f4bb77f78894cee5c0c
Jason Ish <jason@codemonkey.net>
2021-10-28 16:20:55 -0600

    example/tojson: quiet mode and better arg handling

--- examples/tojson.rs
+++ examples/tojson.rs
@@ -1,13 +1,45 @@
-use std::env;
+// Copyright 2021 Jason Ish
+//
+// MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 use std::fs::File;
 use std::io::BufRead;
 use suricata_rule_parser;
 use suricata_rule_parser::NewRule;
 
+#[derive(clap::Parser)]
+struct Opts {
+    /// Quiet mode, only errors and a summary will be printed.
+    #[clap(short, long)]
+    quiet: bool,
+
+    /// List of filenames to parse.
+    filenames: Vec<String>,
+}
+
 fn main() {
-    let filenames: Vec<String> = env::args().skip(1).collect();
+    let opts: Opts = <Opts as clap::Parser>::parse();
     let mut count = 0;
-    for filename in &filenames {
+    let start = std::time::Instant::now();
+    for filename in &opts.filenames {
         let file: File = File::open(filename).unwrap();
         let reader = std::io::BufReader::new(file).lines();
         for line in reader {
@@ -17,13 +49,15 @@ fn main() {
                 }
                 match suricata_rule_parser::parse_rule(&line) {
                     Err(err) => {
-                        println!("Failed to parse rule: {:?} -- {}", err, &line);
+                        eprintln!("Failed to parse rule: {:?} -- {}", err, &line);
                     }
                     Ok((_rem, rule)) => {
                         count += 1;
                         let rule: NewRule = rule.into();
                         let encoded = serde_json::to_string(&rule).unwrap();
-                        println!("{}", encoded);
+                        if !opts.quiet {
+                            println!("{}", encoded);
+                        }
                     }
                 }
             } else {
@@ -31,5 +65,6 @@ fn main() {
             }
         }
     }
-    println!("count: {}", count);
+    let elapsed = start.elapsed();
+    eprintln!("Parsed {} rules in {:?}", count, elapsed);
 }
